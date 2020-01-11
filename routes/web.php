9e2d8f9cd028e1692c6cb55674db328e45e1face
toya.kumagai <sirokumapri@gmail.com>
2020-01-11 00:52:09 +0000

    user login

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,9 @@ Route::resource('tasks', 'TasksController');
 // ユーザ登録のルーティング
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+
+// ログイン認証のルーティング
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
